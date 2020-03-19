0321dcebd993369b66dbb6595d52fea80097100b
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2020-03-19 20:52:02 +0100

    No null for status

--- src/main/kotlin/com/javalinist/controllers/BaseHandler.kt
+++ src/main/kotlin/com/javalinist/controllers/BaseHandler.kt
@@ -4,10 +4,9 @@ import com.javalinist.logic.ResponseStatus
 import io.javalin.http.Context
 
 interface BaseHandler {
-    fun response(cxt: Context, statusCode: Int? = 200, status: ResponseStatus = ResponseStatus.OK, result: Any? = object {}) {
-        if (statusCode != null) {
-            cxt.status(statusCode)
-        }
+    fun response(cxt: Context, statusCode: Int = 200, status: ResponseStatus = ResponseStatus.OK, result: Any? = null) {
+        cxt.status(statusCode)
+
         cxt.json(object {
             val status = status
             val result = result
