a0a57275df60941b282c49d74294a01dc2a67668
Van Nhu Nguyen <nguyen.van.nhu@vnmedia.se>
2020-04-05 09:35:33 +0200

    Create customed ThreadPoolExecutor

--- src/main/kotlin/com/javalinist/handlers/StatusHandler.kt
+++ src/main/kotlin/com/javalinist/handlers/StatusHandler.kt
@@ -4,10 +4,18 @@ import com.javalinist.enums.ResponseStatus
 import io.javalin.http.Context
 import io.javalin.http.Handler
 import java.util.concurrent.CompletableFuture
-import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 class StatusHandler: Handler {
-    val executors = Executors.newFixedThreadPool(2)
+    val executors = ThreadPoolExecutor(
+        1,
+        2,
+        60L,
+        TimeUnit.MILLISECONDS,
+        LinkedBlockingQueue<Runnable>()
+    )
 
     override fun handle(ctx: Context) {
         // response with CompletableFuture
